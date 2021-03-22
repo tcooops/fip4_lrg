084c5a24fcadd99b37076e1379d0519b97cbfa15
Tessa Cooper <55293851+tcooops@users.noreply.github.com>
2021-03-22 15:33:43 -0400

    added favicon

--- templates/links.php
+++ templates/links.php
@@ -1,1 +1,8 @@
 <link rel="stylesheet" href="public/css/main.css">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/site.webmanifest">
+<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#ed2927">
+<meta name="msapplication-TileColor" content="#b91d47">
+<meta name="theme-color" content="#ed2927">
