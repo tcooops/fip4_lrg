6a9b82563c02be07d722c3daaa751d30bcd11f44
Tessa Cooper <55293851+tcooops@users.noreply.github.com>
2021-03-28 22:40:02 -0400

    added schedule template

---
+++ schedule.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LRG</title>
+    <?php include 'templates/links.php'; ?>
+</head>
+<body>
+<main>
+    <div class="main-container">
+        <h1 class="hidden">London Referees Group</h1>
+        <?php include 'templates/header.php'; ?>
+        <div id="app"></div>
+        <?php include 'templates/footer.php'; ?>
+    </div>
+</main>
+<?php include 'templates/bottomLinks.php'; ?>
+</body>
+</html>
