2fa35afa31fee42370c46e17ad892f1d62409cfb
Tessa Cooper <55293851+tcooops@users.noreply.github.com>
2021-03-29 22:43:25 -0400

    lightbox updates

--- admin/admin_register.php
+++ admin/admin_register.php
@@ -104,33 +104,35 @@ if(isset($_POST['submit'])){
         </section>
         
         <footer>
-            <h2 class="hidden">Footer</h2>
-            <div id="footer">
-            <img src="../images/footer_logo.svg" alt="footer logo" id="footer-logo"> 
-            <ul id="footer-nav">
-                <li><a href="../#the-referee">The Referee</a></li>
-                <li><a href="../partners.php">Partners</a></li>
-                <li><a href="../membership.php">Membership</a></li>
-                <li><a href="../join.php">Join Us</a></li> 
-                <li><a href="../contact.php">Contact Us</a></li>       
-            </ul>
-            <ul id="footer-info">
-                <li><a href="">Bylaws</a></li>
-                <li><a href="">COVID-19</a></li>
-                <li><a href="">Supervisors</a></li>
-                <li><a href="">Report a Crime Offence</a></li>
-                <li><a href="">Match Penalty Reports</a></li>
-                <li><a href="">Brand Guidelines</a></li>
-            </ul>
-            <ul id="copyright">
-                <li>Copyright © 2021</li>
-                <li>London Referee Group</li>
-                <li>Proudly Canadian</li>
-            </ul>
-            </div>
-        </footer>
+              <h2 class="hidden">Footer</h2>
+              <div id="footer">
+                <img src="../public/images/footer_logo.svg" alt="footer logo" id="footer-logo"> 
+                <ul id="footer-nav">
+                    <li><a href="../referee.php">The Referee</a></li>
+                    <li><a href="../partners.php">Partners</a></li>
+                    <li><a href="../membership.php">Membership</a></li>
+                    <li><a href="../join.php">Join Us</a></li> 
+                    <li><a href="../contact.php">Contact Us</a></li>       
+                </ul>
+                <ul id="footer-info">
+                    <li><a href="">Bylaws</a></li>
+                    <li><a href="">COVID-19</a></li ff>
+                    <li><a href="">Supervisors</a></li>
+                    <li><a href="">Report a Crime Offence</a></li>
+                    <li><a href="">Match Penalty Reports</a></li>
+                    <li><a href="">Brand Guidelines</a></li>
+                </ul>
+                <ul id="copyright">
+                    <li>Copyright © 2021</li>
+                    <li>London Referee Group</li>
+                    <li>Proudly Canadian</li>
+                </ul>
+              </div>
+          </footer>
     </div>
 </main>
-<script src="../js/burger.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/vue@2.6.12/dist/vue.js"></script>
+<script src="../public/js/burger.js"></script>
+<script src="../public/js/main.js"></script>
 </body>
 </html>
